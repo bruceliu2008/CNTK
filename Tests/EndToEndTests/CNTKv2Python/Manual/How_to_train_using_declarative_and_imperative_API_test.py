d6377d3ab19e2e15d0907a24b2c3c62bb3b791e3
Eldar Akchurin <eldak@microsoft.com>
2017-06-26 19:07:48 +0200

    Fixes

--- Tests/EndToEndTests/CNTKv2Python/Manual/How_to_train_using_declarative_and_imperative_API_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Manual/How_to_train_using_declarative_and_imperative_API_test.py
@@ -12,15 +12,15 @@ sys.path.append(abs_path, "..", "Tutorials")
 
 from nb_helper import get_output_stream_from_cell
 
-notebook = os.path.join(abs_path, "..", "..", "..", "..", "Manual", "CNTK_HOWTO_Training_Session.ipynb")
+notebook = os.path.join(abs_path, "..", "..", "..", "..", "Manual", "How_to_train_using_declarative_and_imperative_API.ipynb")
 
-def test_cntk_HOWTO_training_session_noErrors(nb):
+def test_cntk_how_to_train_no_errors(nb):
     errors = [output for cell in nb.cells if 'outputs' in cell
               for output in cell['outputs'] if output.output_type == "error"]
     assert errors == []
 
 expectedOutput = 'Minibatch\[ 191- 200\]: loss = '
-def test_cntk_HOWTO_training_session_evalCorrect(nb):
+def test_cntk_how_to_train_eval_correct(nb):
     testCells = [cell for cell in nb.cells
                 if cell.cell_type == 'code']
     assert len(testCells) == 4
